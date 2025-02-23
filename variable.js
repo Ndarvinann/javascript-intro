a86ae2ec52a1cabbbd9ab740426bb60bf988f6b8
DarvinAnn <ndarvinann@gmail.com>
2025-02-23 13:13:41 +0300

    using the console

---
+++ variable.js
@@ -0,0 +1,37 @@
+// Comments can make code readable in JS
+// Welcome to CES javascript class
+/*
+Comments can make code readable,
+easy to reuse, and informative.
+*/
+
+// String
+let you = "Darvin Ann";
+let message = 'Hello, world!';
+
+// Boolean
+let isLoggedIn = true;
+let isAdult = false;
+
+// Undefined
+let address; // Declared but not assigned a value
+let city = undefined; // Explicitly assigned undefined
+
+// Null
+let car = null; // Represents the absence of a value
+
+let quantity = null;
+
+console.log( you, message,isLoggedIn,isAdult,address,city,car,quantity)
+
+//8
+let f= "Darvin Ann";
+let g = " Nakigudde";
+let h = " single";
+let i = " uganda";
+let j= 40;
+
+//9
+
+let firstName = "Darvin Ann", lastName = "nakigudde" , maritalstatus="single" , country = "uganda", age = 40;
+
