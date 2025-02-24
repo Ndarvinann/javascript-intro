c03fa3a662af39dd60a96d7b94a107ef7be42651
DarvinAnn <ndarvinann@gmail.com>
2025-02-24 09:59:24 +0300

    if and elseif

--- conditionals.js
+++ conditionals.js
@@ -4,7 +4,23 @@
 //if(condition){
     // part of this code runs if the condition is true.
 //};
+// infinite loop, where it doesnt terminate. used by hackers mainly, or how apps work.
 let number = 6;
 if(number>0){
     console.log(`${number} is a positive integer`)
 }
+ let isRaining = true;
+
+ //let weather ="raining";
+ //if(weather === raining){
+    //console.log(`Remember to carry an umbrella`)};
+  let weatherNow = "ye";
+  if(weatherNow === `rainy`){
+    console.log(`You need an umbrella`)
+  } else if (weatherNow=== `cloudy`){
+    console.log(`you might need an umbrella`)
+  }else if(weatherNow=== `sunny`){
+    console.log(`wear light clothes`)
+  } else{
+    console.log(`No need for an umbrella`)
+  };
