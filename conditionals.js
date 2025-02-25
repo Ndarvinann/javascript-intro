d84d6d084ee1516104b606010e2dd6661b8ecca3
DarvinAnn <ndarvinann@gmail.com>
2025-02-25 08:05:46 +0300

    ternary and switch

--- conditionals.js
+++ conditionals.js
@@ -10,11 +10,11 @@ if(number>0){
     console.log(`${number} is a positive integer`)
 }
  let isRaining = true;
-
+ const prompt = require("prompt-sync")({sigint:true}) // summoning the prompt command. 
  //let weather ="raining";
  //if(weather === raining){
     //console.log(`Remember to carry an umbrella`)};
-  let weatherNow = "ye";
+  let weatherNow = prompt(`Is the weather rainy, sunny or cloudy?`);
   if(weatherNow === `rainy`){
     console.log(`You need an umbrella`)
   } else if (weatherNow=== `cloudy`){
@@ -24,3 +24,16 @@ if(number>0){
   } else{
     console.log(`No need for an umbrella`)
   };
+// read about the switch and the ternary  operators.
+
+
+//Ternary operators
+//short-hand way of writing an if--else statement.called ternary because it takes three operands. a condition, a value toreturn if the condition is true, a value to return if the condition is false.
+// syntax-- condition? expressionIfTrue : expressionIfFalse. 
+//condition validates true or false. "?" is the ternary operator. :; expressionIfTrue: if the condition is true, a value is returned. (:) the colon separates the 'true' or 'false' . (false is the reverse)
+let age = prompt("How old are you?")
+let question = age >=18? "Adult" : "Minor";
+console.log(question); // if the message is greaterthan or equal to 18, return adult, otherwise, return minor. 
+// advantages-- code is shorter and more compact, very readable. dont nest them too deep.
+let score = prompt(`what did you score in Maths?`);
+console.log(score >= 60? "pass" : "fail"); 
