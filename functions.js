7ffb0de1990af563b814d7dbb549c88a7496e632
DarvinAnn <ndarvinann@gmail.com>
2025-03-04 03:03:24 +0300

    if and switch

---
+++ functions.js
@@ -0,0 +1,36 @@
+// two things control the source code decision making. ie if statements, looping, iterations. 
+// the synatx
+//if (condition){
+    // this part of the code runs if the condition is true.
+//};
+let number =6;
+if (number>0){
+    console.log(`${number} is an integer`)
+};
+// string 
+let weather = "raining";
+if (weather = `raining`){
+    console.log(`remember to carry an umbrella`)
+} // use the const-prompt thing to see them in the terminal. 
+else if(weatherNow ===cloudy){
+   console.log(`you might need an umbrella`)
+}
+else {
+    console.log(`no need for an umbrella`)
+};
+// boolean//
+let isRaining = true;
+if (isRaining){
+    console.log(`remember to carry an umbrella`)
+};
+// else
+let numberTwo = 7;
+if(numberTwo){
+console.log(`${numberTwo} is a positive integer`)
+}
+else{
+    console.log(`${numberTwo} is a negative integer`)
+}; 
+// else if
+
+
