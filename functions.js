1f14bea3dec626e186e3018315a0e21554a23844
DarvinAnn <ndarvinann@gmail.com>
2025-03-05 18:55:13 +0300

    functions syntax

--- functions.js
+++ functions.js
@@ -3,6 +3,7 @@
 //if (condition){
     // this part of the code runs if the condition is true.
 //};
+/*
 let number =6;
 if (number>0){
     console.log(`${number} is an integer`)
@@ -31,6 +32,67 @@ console.log(`${numberTwo} is a positive integer`)
 else{
     console.log(`${numberTwo} is a negative integer`)
 }; 
-// else if
+// function, being able to perform an action. create, read, update, delete.  functionality, quality and constraints.
+//constraints is limitations( the browser where it runs.)
 
+/*function add(a,b){ (parameters(values passed into fuctions), arguments(actual values ) return(result of a working thing.))
+return a+b
+}*/
+/*function checkout(){
+    // some logic checkout product online 
+}
+// function call.  
+checkout()
 
+let student = {
+name: "Ann", // name is key, ann is value
+age: 12,
+//method-function written inside an object
+sport: ()=>{
+// Ann plays dodgeball
+    }   
+}
+
+//scope.(globalVariable vs localVar)
+let globalVar = `i am a global variable`;
+function foo(){
+    let localVar = `i am a local variable`;
+}
+foo()// how to call a function
+
+// add two numbers with parameters
+function add(){
+    let a=10;
+    let b = 30;
+    let sum = a+b
+    console.log(sum)
+}
+add(a,b)
+
+// functions without parameters
+function add(x,y){
+    let sum = x+y
+    return sum;
+}
+console.log(add(5,3));
+function square(i){
+    let square = i*i;
+    return square
+}
+square(75);
+
+// expression functions 
+let square = function(j){
+}
+// arrow functions 
+const square =(a)=> (a*a); // this is function short hand.
+square()*/
+
+//relevance to kgl
+//total cost( for all the items ) arrays maybe? 
+function totalCost(produce, pricePerKg, quantity){
+    let totalprice = pricePerKg*quantity;
+    console.log(`the total price of  ${quantity} kg of  ${produce} is ${totalprice} Ugx` );
+}
+totalCost(`maize`,3000, 45);
+totalCost(`beans`,3000, 115);
