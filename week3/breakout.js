617516ecd05e5c81e5d8d0b7a743ae9689e4edb8
DarvinAnn <ndarvinann@gmail.com>
2025-03-06 18:06:34 +0300

    last number

--- week3/breakout.js
+++ week3/breakout.js
@@ -60,3 +60,50 @@ console.log(graphs);
 
 //or 
 // direct property assignment.
+graphs.forEach((paragraph , index ) => {
+    paragraph. className= (`para-class-${index+1}`);
+})
+console.log(graphs);
+
+//or
+// using id property
+graphs.forEach((paragraph , index ) => {
+    paragraph. id = `new-id-${index+1}`;
+})
+console.log(graphs);
+
+//start of exercise 2.
+
+//1. Change style of each paragraph using JavaScript(eg. color, background,border, font-size, font-family
+// from Ln 28
+graphs.forEach((paragraph, index)=>{ // for each paragraph and its index, do this. 
+    paragraph.style.color = 'darkblue'; //change color
+    paragraph.style.backgroundColor = 'lightyellow';
+    paragraph.style.border = '2px solid green';
+    paragraph.style.fontsize='${16+index*2}px'
+    paragraph.style.fontFamily = 'Arial , sans-serif';
+    paragraph.style.padding ='10px';
+    paragraph.style.margin = '5px';
+    paragraph .style.textAlign ='right';
+    paragraph.style.fontWeight ='bold';
+});
+console.log(graphs);
+//Select all paragraphs and loop through each elements and give the first  and third paragraph a color of green, and the second and the fourth paragraph a red color.
+  graphs.forEach((paragraph , index)=>{
+    if(index ===0 || index===2) // the either or concept for true or false.
+        {
+            paragraph.style.color = `green`;
+    }
+    else if  (index ===1 || index ===3) // else didnt work because it works the way default works in switches.
+    {
+        paragraph.style.color ='red';
+    }
+  });
+  //Set text content, id and class to each paragraph
+  graphs.forEach((paragraph , index)=>{
+    paragraph.textContent = `paragraph ${index+1}`; // text content.
+    paragraph.setAttribute(`id`, `paragraph-${index + 1}`); 
+    paragraph.className =`para-class-${index+1}`;
+  });
+
+  // in relation to KGL, using Dom manipulation.
