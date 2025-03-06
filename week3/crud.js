2777bd0412a387b723cfb48f845f248506f1fc20
DarvinAnn <ndarvinann@gmail.com>
2025-03-06 18:10:58 +0300

    initial commit

---
+++ week3/crud.js
@@ -0,0 +1,21 @@
+// function to create an element in the DOM. 
+
+function create(){
+    const paragraph= document.createElement('button')
+    paragraph.innerHTML=`what is your name`
+    document.body.appendChild(paragraph)
+    //console.log("clickbutton")
+}
+
+// function to read an element in the DOM. 
+function read(){
+    const readQn = document.getElementById('readQn')
+}
+// function to update  an element in the DOM. 
+function update(){
+    const updateQn = document. getElementById('updateQn')
+}
+// function to delete an element in the DOM. 
+function delitus (){
+    const deleteQn = document. getElementById('deleteQn')
+}
