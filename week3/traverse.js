99451be6cde0cd2274a46a6674ebd174c1374cc3
DarvinAnn <ndarvinann@gmail.com>
2025-03-06 18:08:58 +0300

    initial commit

---
+++ week3/traverse.js
@@ -0,0 +1,8 @@
+//parents, siblings, children
+//traversing helps navigate the dom
+let parent = document.getElementById(`produce`)
+console.log(parent.children)
+console.log(parent.firstElementChild.textContent)
+console.log(parent.lastElementChild.textContent)
+
+// assgn. write 4 kgl functions for creating, deleting, edit buttons. 
