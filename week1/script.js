88eda935786e4bc7dca9291163a6e129c613ebc5
DarvinAnn <ndarvinann@gmail.com>
2025-03-13 22:16:29 +0300

    initial commit

---
+++ week1/script.js
@@ -0,0 +1,37 @@
+var y = 30;
+
+console.log("var")
+
+console.log(y)
+ 
+console.log("Hello world")
+  
+
+let x = 10;
+
+  console.log(`The value of x is ${x}.`);// backticks for variables within strings.
+  let age = 25;
+  console.log(age)
+
+  let price = 10;
+  let quantity = 7;
+  console.log(price * quantity); // can do simple maths equations
+
+  let a = 45;
+  console.log( typeof a); //  use typeof 
+
+  let b = 'Angela';
+  console.log( typeof b);
+//no.10
+  let myAge = 25;
+let yourAge = 30;
+
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old.`);
+// free-code camp.
+console.log("Let's build a pyramid using JavaScript basics:");
+console.log("    #    ");
+console.log("   ###   ");
+console.log("  #####  ");
+console.log(" ####### ");
+console.log("#########");
